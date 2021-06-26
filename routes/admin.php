e673de1d9a91c0276cbc0b6956d2fee80177e0c1
Kerem BAŞARAN <kerembasaran14@gmail.com>
2021-06-26 16:31:25 +0300

    Fortify paket denetleyicilerini kullanan admin için oturum açma özelliği

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,15 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
 
-Route::prefix('admin')->group(function (){
+Route::prefix('admin')->name('admin.')->group(function (){
     Route::view('/login', 'admin.auth.login')->name('login');
+
+    $limiter = config('fortify.limiters.login');
+
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+        ->middleware(array_filter([
+            'guest:admin',
+            $limiter ? 'throttle:' . $limiter : null,
+        ]));
 });
